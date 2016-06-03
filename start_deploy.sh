cfb2cc510b8673772017f2a9b4d69a217e1a59f5
zrks <marcis.veveris@outlook.com>
2016-06-03 11:58:44 +0000

    Change command in deploy launcher script

--- start_deploy.sh
+++ start_deploy.sh
@@ -3,4 +3,4 @@
 set -e
 openvpn --config client.ovpn & export OVPN_ID=$!
 sleep 30
-fab test_user -H ec2-user@10.10.10.130 -i turner_dev_sandbox
+fab deploy -H ec2-user@10.10.10.130 -i turner_dev_sandbox
