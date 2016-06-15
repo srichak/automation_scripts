cab9aeac99e2aeadb46bfd918571f990bc00fe3c
U-DIR\dmitrii.kvaskov <dmitrii.kvaskov@LVALTP1651.dir.svc.accenture.com>
2016-06-15 18:17:45 +0300

    added copy and restore conf folder tasks

--- fabfile.py
+++ fabfile.py
@@ -23,8 +23,7 @@ def make_timestamp():
 @task
 def make_backup():
     with cd('/product/apache2/'):
-        sudo(
-            'tar -zcvf demo44_{}_test.tar.gz demo44/'.format(make_timestamp()))
+        sudo('tar -zcvf demo44_{}_test.tar.gz demo44/'.format(make_timestamp()))
 
 
 @task
@@ -58,6 +57,16 @@ def copy_token():
     with cd('/product/apache2/demo44/'):
         sudo('cp SDK/lib/token/'+initial_token_name+' SDK/lib/token/domain_token.dat')
 
+@task
+def copy_conf_to_tmp():
+    with cd('/product/apache2/demo44/SDK/com/accenture/avs/sdk/'):
+        sudo('cp -r conf/ /tmp')
+        sudo('tar -zcvf conf_{}_bkp.tar.gz /tmp/conf/'.format(make_timestamp()))
+
+@task
+def restore_conf_from_tmp():
+    with cd('/product/apache2/demo44/SDK/com/accenture/avs/sdk/conf/'):
+        sudo('cp -r /tmp/conf/* .')
 
 @task
 def restart_apache():
@@ -82,8 +91,10 @@ def deploy():
     pull_package()
     extract_package()
     make_backup()
+    copy_conf_to_tmp()
     copy_app()
     copy_token()
+    restore_conf_from_tmp()
     fix_permissions()
     restart_apache()
     clear_cache()
