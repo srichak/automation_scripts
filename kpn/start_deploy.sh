cc9546ccb228f7755cf60f1058ee05d68999c263
U-DIR\dmitrii.kvaskov <dmitrii.kvaskov@LVALTP1651.dir.svc.accenture.com>
2016-07-04 16:43:06 +0300

    changed startdeploy.sh added passwd parameter

--- kpn/start_deploy.sh
+++ kpn/start_deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 cat pass | openconnect ${VPN_SERVER_IP} --authgroup=${VPN_AUTH_GROUP} --user=${VPN_USER} --no-cert-check -b --passwd-on-stdin
-fab deploy -H ${SSH_USER}@${TARGET_HOST} #-i kpn_ssh_key
+fab deploy -H ${SSH_USER}@${TARGET_HOST} -p ${SSH_USER_PASSWORD} #-i kpn_ssh_key
 kill $(ps axw | grep openconnect | awk '{print$1}')
