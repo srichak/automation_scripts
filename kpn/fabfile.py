141e960c35a924d365710df6dc685e4a5789e19d
U-DIR\dmitrii.kvaskov <dmitrii.kvaskov@LVALTP1651.dir.svc.accenture.com>
2016-07-05 16:37:03 +0300

    added no_sudo task into fabfile, changed start_deploy to use no_sudo task 1

--- kpn/fabfile.py
+++ kpn/fabfile.py
@@ -17,7 +17,7 @@ def test_timestamp():
 
 @task
 def make_timestamp():
-    return datetime.datetime.now().strftime('%Y%m%d')
+    return datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
 
 
 @task
@@ -105,14 +105,14 @@ def deploy_no_sudo():
 @task
 def make_backup_no_sudo():
     with cd('/product/apache2/'):
-        run('tar -zcvf '+deployment_name+'_{}_bkp.tar.gz '+deployment_name+'/'.format(make_timestamp()))
+        run('tar -zcvf +deployment_name+_{}_bkp.tar.gz '+deployment_name+'/'.format(make_timestamp()))
 
 
 
 @task
 def extract_package_no_sudo():
     with cd('/tmp'):
-        run('rm -rf dist*/')
+        run('rm -rf pctv*/')
         run('tar -xzf '+deployment_package)
 
 
@@ -125,20 +125,20 @@ def fix_permissions_no_sudo():
 @task
 def copy_app_no_sudo():
     with cd('/product/apache2/'+deployment_name+'/'):
-        run('rm -rf angular/ assets/ static/ index.html SDK/')
-        run('cp -r /tmp/dist/apache/* .')
+        run('rm -rf config/ css/ fonts/ images/ index.html/ js/ lib/ templates/ version.json')
+        run('cp -r /tmp/pctv/dist/* .')
 
 
 @task
 def copy_conf_to_tmp_no_sudo():
-    with cd('/product/apache2/'+deployment_name+'/SDK/com/accenture/avs/sdk/'):
-        run('cp -r conf/ /tmp')
-        run('tar -zcvf conf_{}_bkp.tar.gz /tmp/conf/'.format(make_timestamp()))
+    with cd('/product/apache2/'+deployment_name+'/'):
+        run('cp -r json/ /tmp')
+        run('tar -zcvf json_{}_bkp.tar.gz /tmp/json/'.format(make_timestamp()))
 
 @task
 def restore_conf_from_tmp_no_sudo():
-    with cd('/product/apache2/'+deployment_name+'/SDK/com/accenture/avs/sdk/conf/'):
-        run('cp -r /tmp/conf/* .')
+    with cd('/product/apache2/'+deployment_name+'/'):
+        run('cp -r /tmp/json/* .')
 
 @task
 def restart_apache_no_sudo():
