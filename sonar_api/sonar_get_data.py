9063a5773f08ba1b52236d5e3550641258e883e5
Kristians Bernatovics <k.bernatovics@accenture.com>
2016-09-05 15:59:32 +0300

    Edited python script

--- sonar_api/sonar_get_data.py
+++ sonar_api/sonar_get_data.py
@@ -1,1 +1,12 @@
+from sonarqube_api import SonarAPIHandler
+
 print 'python output test'
+
+#self, host, port, user, password, base_path, token
+h = SonarAPIHandler(user='k.bernatovics', password='mae6oi6O',
+host='https://avsdevops.accenture.com/sonar/')
+h.validate_authentication();
+#for project in h.get_resources_full_data(metrics=['coverage', 'violations']):
+    # do something with project data...
+    #print project.coverage
+    #print project.violations
