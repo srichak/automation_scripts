0ed49980bdb47a6ba4ccde26a7640413b8db6efb
Kristians Bernatovics <k.bernatovics@accenture.com>
2016-10-31 15:07:06 +0200

    edit start_deploy.sh

--- kpn_m3/start_deploy.sh
+++ kpn_m3/start_deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 cat pass | openconnect ${VPN_SERVER_IP} --authgroup=${VPN_AUTH_GROUP} --user=${VPN_USER} --no-cert-check -b --passwd-on-stdin
-fab deploy_no_sudo -H ${SSH_USER}@${TARGET_HOST} -p ${SSH_USER_PASSWORD}
+fab deploy_dcq -H ${SSH_USER}@${TARGET_HOST} -p ${SSH_USER_PASSWORD}
 kill $(ps axw | grep openconnect | awk '{print$1}')
