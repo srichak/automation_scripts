4edd4a641bf20b08c35433193da62ab04bd0e7c2
zrks <marcis.veveris@outlook.com>
2016-11-14 10:18:02 +0000

    Check file mod

--- telus_poc/start_deploy.sh
+++ telus_poc/start_deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -e
-# openvpn --auth-user-pass=pass --config client.ovpn & export OVPN_ID=$!
 
-cat pass | openconnect ${VPN_SERVER_IP} --user=${VPN_USER} --no-cert-check -b --passwd-on-stdin
+openvpn --auth-user-pass=pass --config client.ovpn & export OVPN_ID=$!
+
+# cat pass | openconnect ${VPN_SERVER_IP} --user=${VPN_USER} --no-cert-check -b --passwd-on-stdin
 
 sleep 30
 
