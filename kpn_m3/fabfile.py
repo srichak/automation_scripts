b7748a460bde62e6d362a8d3f05bce13b3e0859b
Kristians Bernatovics <k.bernatovics@accenture.com>
2016-11-16 16:03:34 +0200

    added nexus path as extra var for ansible command in kpn_m3/fabfile and removed expose port from Dockerfile

--- kpn_m3/fabfile.py
+++ kpn_m3/fabfile.py
@@ -5,18 +5,17 @@ from fabric.context_managers import cd, settings
 
 ansible_path = os.getenv('ANSIBLE_PATH')
 invetory_file_path = os.getenv('INVENTORY_FILE_PATH')
+ansible_tags = os.getenv('ANSIBLE_TAGS')
 ansible_playbook_name = os.getenv('ANSIBLE_PLAYBOOK_NAME')
 vault_pass_file = os.getenv('VAULT_PASS_FILE')
-ansible_tags = os.getenv('ANSIBLE_TAGS')
-
+extra_vars = os.getenv('EXTRA_VARS')
 
 @task
 def test_ansible():
     sudo('ansible --list-hosts all')
 
-
 @task
 def deploy_dcq():
-    with cd(ansible_path):
-        sudo('ansible-playbook -vvvv -i ' + invetory_file_path + ' ' +
-             ansible_tags + ' ' + ansible_playbook_name + ' ' + vault_pass_file)
+	with cd(ansible_path):
+		sudo('git pull')
+		sudo('ansible-playbook -vvvv -i '+invetory_file_path+' '+ansible_tags+' '+ansible_playbook_name+' '+vault_pass_file+' '+extra_vars)
