c78681e3c4257c7f6fae9d691af3fedef7b16009
dmitrijs.jakovlevs <dmitrijs.jakovlevs@accenture.com>
2016-11-24 15:23:52 +0000

    Update start_deploy.sh

--- start_deploy.sh
+++ start_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-openvpn --auth-user-pass=pass & export OVPN_ID=$! # --config client.ovpn & export OVPN_ID=$!
+openvpn --config client.ovpn & export OVPN_ID=$! #--auth-user-pass=pass & export OVPN_ID=$!
 sleep 30
 fab deploy -H ec2-user@10.10.10.130 -i turner_dev_sandbox
