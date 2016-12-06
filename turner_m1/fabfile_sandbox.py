5e26548f4d711565b141e5d47d070b2bd1a63208
U-DIR\dmitrijs.jakovlevs <dmitrijs.jakovlevs@LVAGPLTP1890.dir.svc.accenture.com>
2016-12-06 09:50:30 +0200

    separate folder created for turner m1 scripts

---
+++ turner_m1/fabfile_sandbox.py
@@ -0,0 +1,92 @@
+import datetime
+from fabric.api import sudo, put, task, run
+from fabric.context_managers import cd, settings
+
+
+@task
+def test_user():
+    sudo('whoami', user='custapache')
+
+
+@task
+def test_timestamp():
+    print('demo44_{}'.format(make_timestamp()))
+
+
+@task
+def make_timestamp():
+    return datetime.datetime.now().strftime('%Y%m%d')
+
+
+@task
+def make_backup():
+    with cd('/product/apache2/'):
+        sudo(
+            'tar -zcvf demo44_{}_test.tar.gz demo44/'.format(make_timestamp()))
+
+
+@task
+def pull_package():
+    with cd('/tmp'):
+        put('app.tar.gz', './')
+
+
+@task
+def extract_package():
+    with cd('/tmp'):
+        sudo('rm -rf dist*/')
+        sudo('tar -xzf app.tar.gz')
+
+
+@task
+def fix_permissions():
+    with cd('/product/apache2/'):
+        sudo('chown -R custapache:apps demo44/')
+
+
+@task
+def copy_app():
+    with cd('/product/apache2/demo44/'):
+        sudo('rm -rf angular/ assets/ static/ index.html SDK/')
+        sudo('cp -r /tmp/dist/apache/* .')
+
+
+@task
+def copy_token():
+    with cd('/product/apache2/demo44/'):
+        sudo('cp SDK/lib/token/accenture-turner-dev.dat SDK/lib/token/domain_token.dat')
+
+
+@task
+def copy_token_sandbox():
+    with cd('/product/apache2/demo44/'):
+        sudo('cp SDK/lib/token/accenture-turner-sandbox.dat SDK/lib/token/domain_token.dat')
+
+
+@task
+def restart_apache():
+    with cd('/product/apache2/bin/'):
+        sudo('./apachectl restart', user='custapache')
+
+
+@task
+def clear_cache():
+    with cd('/product/varnish/bin/'):
+        run('./varnishadm \'ban req.url ~ \".\"\'')
+
+
+@task
+def ls():
+    with cd('/product/apache2/demo44/'):
+        sudo('ls')
+
+
+@task
+def deploy():
+    pull_package()
+    extract_package()
+    copy_app()
+    copy_token_sandbox()
+    fix_permissions()
+    restart_apache()
+    clear_cache()
---
+++ turner_m1/start_deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+openvpn --config client.ovpn & export OVPN_ID=$! #--auth-user-pass=pass & export OVPN_ID=$!
+sleep 30
+fab ls -H ec2-user@10.10.10.130 -i turner_dev_sandbox
