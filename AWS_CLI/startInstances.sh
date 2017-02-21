df1f7cb12af9aea1a4c7ff0fb02d3d22f1eef0a2
Herrera <erick.herrera@accenture.com>
2017-02-21 16:21:43 -0600

    fixed StartInstances script

--- AWS_CLI/startInstances.sh
+++ AWS_CLI/startInstances.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 set -ex
-CUST=rh2chain#!/bin/bash
-set -ex
-CUST=rh2chain
+CUST=rh2chain#
 STATE=stopped
 
 for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
         echo "suspending auto-scaling group $ASG..";
-        aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
-#       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-done
+        				aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
+						#       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
+					done
 
 for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "stopping instance $BOX..";
-        aws start-instances-instances --instance-ids $BOX;
-#       aws ec2 start-instances --instance-ids $BOX;
-done
+        		aws start-instances-instances --instance-ids $BOX;
+				#       aws ec2 start-instances --instance-ids $BOX;
+			done
 STATE=stopped
 
 for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
         echo "suspending auto-scaling group $ASG..";
-        aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
-#       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-done
+      		  	aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
+				#       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
+			done
 
-for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "stopping instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
-#       aws ec2 start-instances --instance-ids $BOX;
-done
+			for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+       			 	echo "stopping instance $BOX..";
+     				  		 aws ec2 start-instances --instance-ids $BOX;
+							#       aws ec2 start-instances --instance-ids $BOX;
+						done
