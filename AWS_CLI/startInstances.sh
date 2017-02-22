8fab48b89492f7c3084a86a62f560364ca3892c2
rudolfs.horsts <rudolfs.horsts@accenture.com>
2017-02-22 11:44:38 -0500

    Let execute without filter to start/stop ALL environments.

--- AWS_CLI/startInstances.sh
+++ AWS_CLI/startInstances.sh
@@ -3,27 +3,30 @@ set -ex
 CUST=${CUSTOMER_TAG}
 STATE=stopped
 
-for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
-        echo "suspending auto-scaling group $ASG..";
-        				aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
-						#       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-					done
+if [ -n "$CUST" ]; then
+    # work with only specified environment
 
-for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "stopping instance $BOX..";
-        		aws start-instances-instances --instance-ids $BOX;
-				#       aws ec2 start-instances --instance-ids $BOX;
-			done
-STATE=stopped
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+        echo "starting instance $BOX..";
+        aws start-instances-instances --instance-ids $BOX;
+    done
 
-for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
-        echo "suspending auto-scaling group $ASG..";
-      		  	aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
-				#       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-			done
+    for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
+        echo "releasing auto-scaling group $ASG..";
+        aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
+    done
 
-			for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-       			 	echo "stopping instance $BOX..";
-     				  		 aws ec2 start-instances --instance-ids $BOX;
-							#       aws ec2 start-instances --instance-ids $BOX;
-						done
+else
+    # work with all environments
+
+    for BOX in `aws ec2 describe-instances --filters "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+        echo "starting instance $BOX..";
+        aws ec2 start-instances --instance-ids $BOX;
+    done
+
+    for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text`; do
+        echo "releasing auto-scaling group $ASG..";
+      	aws autoscaling resume-processes-processes --auto-scaling-group-name $ASG;
+    done
+
+fi
--- AWS_CLI/stopInstances.sh
+++ AWS_CLI/stopInstances.sh
@@ -3,28 +3,30 @@ set -ex
 CUST=${CUSTOMER_TAG}
 STATE=running
 
-for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
-        echo "suspending auto-scaling group $ASG..";
-                        aws autoscaling suspend-processes --auto-scaling-group-name $ASG;
-                        #       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-                    done
+if [ -n "$CUST" ]; then
+    # work with only specified environment
 
-for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
+        echo "suspending auto-scaling group $ASG..";
+        aws autoscaling suspend-processes --auto-scaling-group-name $ASG;
+    done
+
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "stopping instance $BOX..";
-                aws ec2 stop-instances --instance-ids $BOX;
-                #       aws ec2 start-instances --instance-ids $BOX;
-            done
-STATE=running
+        aws ec2 stop-instances --instance-ids $BOX;
+    done
 
-for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
+else
+    # work with all environments
+
+    for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text`; do
         echo "suspending auto-scaling group $ASG..";
-                aws autoscaling suspend-processes --auto-scaling-group-name $ASG;
-                #       aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-            done
+        aws autoscaling suspend-processes --auto-scaling-group-name $ASG;
+    done
 
-            for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-                    echo "stopping instance $BOX..";
-                            aws ec2 stop-instances --instance-ids $BOX;
-                            #       aws ec2 start-instances --instance-ids $BOX;
-                        done
-                                                
+    for BOX in `aws ec2 describe-instances --filters "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+        echo "stopping instance $BOX..";
+        aws ec2 stop-instances --instance-ids $BOX;
+    done
+
+fi
