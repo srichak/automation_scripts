acea6195f8a3afd8f38de122aa982f725d4b0db8
l.lemberskis <l.lemberskis@accenture.com>
2017-03-03 16:12:05 +0000

    Update start_deploy.sh

--- AWS_CLI/start_deploy.sh
+++ AWS_CLI/start_deploy.sh
@@ -6,9 +6,11 @@ echo "Sending to instances command to "${SET_INSTANCES_STATUS}
 
 if [ "${SET_INSTANCES_STATUS}" = start ]; then
     echo "Starting..."
-    ./startInstances.sh
+    ./startInstances.sh &&
+    ./dbserviceup.sh
 else
     echo "Stoppping..."
+    ./dbservicedown.sh &&
     ./stopInstances.sh
 fi
 
