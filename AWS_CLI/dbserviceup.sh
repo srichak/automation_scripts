4a430b142e29b3ad3556c184e3a4ae919a41b815
l.lemberskis <l.lemberskis@accenture.com>
2017-03-06 15:51:46 +0000

    Update dbserviceup.sh

--- AWS_CLI/dbserviceup.sh
+++ AWS_CLI/dbserviceup.sh
@@ -3,7 +3,7 @@ export CUST=${CUSTOMER_TAG}
 
 # SSH Config Generating function
 sshconf() {
-printf "Host $1\n\tUser ec2-user\n\tIdentityFile ~/.ssh/id_rsa\n\tStrictHostKeyChecking=no\n\tUserKnownHostsFile =/dev/null\n\tPort 22\n\tProxyCommand ssh -W %%h:%%p -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ec2-user@$2\n"
+printf "Host $1\n\tUser ec2-user\n\tIdentityFile $3\n\tStrictHostKeyChecking=no\n\tUserKnownHostsFile =/dev/null\n\tPort 22\n\tProxyCommand ssh -W %%h:%%p -o StrictHostKeyChecking=no -i $3 ec2-user@$2\n"
 }
 
 if [[ $CUST = "devops6" ]]
@@ -19,7 +19,7 @@ echo $bastion
 aws ec2 describe-instances --filters "Name=tag:customer,Values=devops6" "Name=tag:Name,Values=devops6galera*" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`],NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress]' --output text | awk '$1=$1' | sed -e 's/Name\ /Name=/g' | tr '\n' ' ' | sed -e 's/\Name=/\n/g' | awk '$1=$1' | sort -nk1 | while read line;
 do
 
-sshconf `echo $line | awk '{ print $2}'` $bastion >> /tmp/ssh;
+sshconf `echo $line | awk '{ print $2}'` $bastion /devops6sshkey.pem >> /tmp/ssh;
 ansible all -i "`echo $line | awk '{ print $2}'`," -b -m service -a "name=mysqld state=started" ;
 sleep 3;
 done
@@ -27,7 +27,7 @@ done
 # Starting Magento DB instances in correct order
 aws ec2 describe-instances --filters "Name=tag:customer,Values=devops6" "Name=tag:Name,Values=devops6database*" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`],NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress]' --output text | awk '$1=$1' | sed -e 's/Name\ /Name=/g' | tr '\n' ' ' | sed -e 's/\Name=/\n/g' | awk '$1=$1' | sort -nk1 | while read line;
 do
-sshconf `echo $line | awk '{ print $2}'` $bastion >> /tmp/ssh;
+sshconf `echo $line | awk '{ print $2}'` $bastion /devops6sshkey.pem >> /tmp/ssh;
 ansible all -i "`echo $line | awk '{ print $2}'`," -b -m service -a "name=mysqld state=started" ;
 sleep 3;
 done
@@ -35,7 +35,7 @@ done
 # Starting Cassandra DB instances in correct order
 aws ec2 describe-instances --filters "Name=tag:customer,Values=devops6" "Name=tag:Name,Values=devops6bookmark*" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`],NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress]' --output text | awk '$1=$1' | sed -e 's/Name\ /Name=/g' | tr '\n' ' ' | sed -e 's/\Name=/\n/g' | awk '$1=$1' | sort -nk1 | while read line;
 do
-sshconf `echo $line | awk '{ print $2}'` $bastion >> /tmp/ssh;
+sshconf `echo $line | awk '{ print $2}'` $bastion /devops6sshkey.pem >> /tmp/ssh;
 ansible all -i "`echo $line | awk '{ print $2}'`," -b -m service -a "name=cassandra state=started" ;
 sleep 3;
 done
@@ -52,7 +52,7 @@ bastion=`aws ec2 describe-instances --filters "Name=tag:customer,Values=devops4"
 aws ec2 describe-instances --filters "Name=tag:customer,Values=devops4" "Name=tag:Name,Values=devops4galera*" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`],NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress]' --output text | awk '$1=$1' | sed -e 's/Name\ /Name=/g' | tr '\n' ' ' | sed -e 's/\Name=/\n/g' | awk '$1=$1' | sort -nk1 | while read line;
 do
 
-sshconf `echo $line | awk '{ print $2}'` $bastion >> /tmp/ssh;
+sshconf `echo $line | awk '{ print $2}'` $bastion /devops4sshkey.pem >> /tmp/ssh;
 ansible all -i "`echo $line | awk '{ print $2}'`," -b -m service -a "name=mysqld state=started" ;
 sleep 3;
 done
@@ -60,7 +60,7 @@ done
 # Starting Magento DB instances in correct order
 aws ec2 describe-instances --filters "Name=tag:customer,Values=devops4" "Name=tag:Name,Values=devops4database*" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`],NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress]' --output text | awk '$1=$1' | sed -e 's/Name\ /Name=/g' | tr '\n' ' ' | sed -e 's/\Name=/\n/g' | awk '$1=$1' | sort -nk1 | while read line;
 do
-sshconf `echo $line | awk '{ print $2}'` $bastion >> /tmp/ssh;
+sshconf `echo $line | awk '{ print $2}'` $bastion /devops4sshkey.pem >> /tmp/ssh;
 ansible all -i "`echo $line | awk '{ print $2}'`," -b -m service -a "name=mysqld state=started" ;
 sleep 3;
 done
@@ -68,7 +68,7 @@ done
 # Starting Cassandra DB instances in correct order
 aws ec2 describe-instances --filters "Name=tag:customer,Values=devops4" "Name=tag:Name,Values=devops4bookmark*" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`],NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress]' --output text | awk '$1=$1' | sed -e 's/Name\ /Name=/g' | tr '\n' ' ' | sed -e 's/\Name=/\n/g' | awk '$1=$1' | sort -nk1 | while read line;
 do
-sshconf `echo $line | awk '{ print $2}'` $bastion >> /tmp/ssh;
+sshconf `echo $line | awk '{ print $2}'` $bastion /devops4sshkey.pem >> /tmp/ssh;
 ansible all -i "`echo $line | awk '{ print $2}'`," -b -m service -a "name=cassandra state=started" ;
 sleep 3;
 done
