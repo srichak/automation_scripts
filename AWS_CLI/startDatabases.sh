b16a6b0792186862dfd0a4376609c0850ce6b68b
rudolfs.horsts <rudolfs.horsts@accenture.com>
2017-03-09 12:49:46 +0000

    Update startDatabases.sh - fix filter for galera servers

--- AWS_CLI/startDatabases.sh
+++ AWS_CLI/startDatabases.sh
@@ -7,7 +7,11 @@ STATE=stopped
 
 if [ -n "$CUST" ]; then
     # work with only specified environment
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera*,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+        echo "starting instance $BOX..";
+        aws ec2 start-instances --instance-ids $BOX;
+    done
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_slave,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
     done
@@ -21,7 +25,11 @@ if [ -n "$CUST" ]; then
     done
 else
     # work with all environments
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera*,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+        echo "starting instance $BOX..";
+        aws ec2 start-instances --instance-ids $BOX;
+    done
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_slave,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
     done
