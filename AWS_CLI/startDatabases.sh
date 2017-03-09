55a0db5f525479b57a3f6aaec01e8c54a6cbf0a9
rudolfs.horsts <rudolfs.horsts@accenture.com>
2017-03-09 13:01:00 +0000

    Update startDatabases.sh

--- AWS_CLI/startDatabases.sh
+++ AWS_CLI/startDatabases.sh
@@ -7,6 +7,10 @@ STATE=stopped
 
 if [ -n "$CUST" ]; then
     # work with only specified environment
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:Name,Values=${CUST}ansible" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+        echo "starting instance $BOX..";
+        aws ec2 start-instances --instance-ids $BOX;
+    done
     for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
