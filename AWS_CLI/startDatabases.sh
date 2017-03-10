83da6ff4aa32e576d4778b3f4531072704d17b4b
rudolfs.horsts <rudolfs.horsts@accenture.com>
2017-03-10 16:08:50 +0000

    Update startDatabases.sh

--- AWS_CLI/startDatabases.sh
+++ AWS_CLI/startDatabases.sh
@@ -6,42 +6,44 @@ STATE=stopped
 # start database - Galera, Cassandra, Magento - servers
 
 if [ -n "$CUST" ]; then
-    # work with only specified environment
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:Name,Values=${CUST}ansible" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "starting instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
-    done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "starting instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
-    done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_slave,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "starting instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
-    done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:magento_db,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "starting instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
-    done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:bookmark,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "starting instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
+    for customer in ${CUST//,/ }; do
+        # work with only specified environment
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=tag:Name,Values=${customer}ansible" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "starting instance $BOX..";
+            aws ec2 start-instances --instance-ids $BOX;
+        done
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "starting instance $BOX..";
+            aws ec2 start-instances --instance-ids $BOX;
+        done
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=tag:galera_slave,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "starting instance $BOX..";
+            aws ec2 start-instances --instance-ids $BOX;
+        done
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=tag:magento_db,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "starting instance $BOX..";
+            aws ec2 start-instances --instance-ids $BOX;
+        done
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=tag:bookmark,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "starting instance $BOX..";
+            aws ec2 start-instances --instance-ids $BOX;
+        done
     done
 else
     # work with all environments
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:galera_master,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
     done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:galera_slave,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:galera_slave,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
     done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:magento_db,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:magento_db,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
     done
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=tag:bookmark,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+    for BOX in `aws ec2 describe-instances --filters "Name=tag:bookmark,Values=tag_Role" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
         echo "starting instance $BOX..";
         aws ec2 start-instances --instance-ids $BOX;
     done
