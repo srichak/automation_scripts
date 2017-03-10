cfa51ff5bcb1d174510f7b9b3b9e2338c868e61a
rudolfs.horsts <rudolfs.horsts@accenture.com>
2017-03-10 16:11:18 +0000

    Update startInstances.sh

--- AWS_CLI/startInstances.sh
+++ AWS_CLI/startInstances.sh
@@ -4,18 +4,19 @@ CUST=${CUSTOMER_TAG}
 STATE=stopped
 
 if [ -n "$CUST" ]; then
-    # work with only specified environment
+    for customer in ${CUST//,/ }; do
+        # work with only specified environment
+        
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "starting instance $BOX..";
+            aws ec2 start-instances --instance-ids $BOX;
+        done
 
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "starting instance $BOX..";
-        aws ec2 start-instances --instance-ids $BOX;
+        for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep ${customer}`; do
+            echo "releasing auto-scaling group $ASG..";
+            aws autoscaling resume-processes --auto-scaling-group-name $ASG;
+        done
     done
-
-    for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
-        echo "releasing auto-scaling group $ASG..";
-        aws autoscaling resume-processes --auto-scaling-group-name $ASG;
-    done
-
 else
     # work with all environments
 
