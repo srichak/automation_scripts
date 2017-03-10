839e56141d1a1b045d10988aee7ae05c6f07228b
rudolfs.horsts <rudolfs.horsts@accenture.com>
2017-03-10 16:17:16 +0000

    Update stopInstances.sh

--- AWS_CLI/stopInstances.sh
+++ AWS_CLI/stopInstances.sh
@@ -4,18 +4,22 @@ CUST=${CUSTOMER_TAG}
 STATE=running
 
 if [ -n "$CUST" ]; then
-    # work with only specified environment
 
-    for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep $CUST`; do
-        echo "suspending auto-scaling group $ASG..";
-        aws autoscaling suspend-processes --auto-scaling-group-name $ASG;
+    for customer in ${CUST//,/ }; do
+        # work with only specified environment
+
+        for ASG in `aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].[AutoScalingGroupName]' --output=text | grep ${customer}`; do
+            echo "suspending auto-scaling group $ASG..";
+            aws autoscaling suspend-processes --auto-scaling-group-name $ASG;
+        done
+
+        for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=${customer}" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
+            echo "stopping instance $BOX..";
+            aws ec2 stop-instances --instance-ids $BOX;
+        done
+        
     done
-
-    for BOX in `aws ec2 describe-instances --filters "Name=tag:customer,Values=$CUST" "Name=instance-state-name,Values=$STATE" --query 'Reservations[*].Instances[*].[InstanceId]' --output text`; do
-        echo "stopping instance $BOX..";
-        aws ec2 stop-instances --instance-ids $BOX;
-    done
-
+    
 else
     # work with all environments
 
