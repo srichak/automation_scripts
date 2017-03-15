a9838c6d3f173068544b1c41ebc775a4397e45b5
k.bernatovics <k.bernatovics@accenture.com>
2017-03-15 14:42:03 +0000

    Upload new file

---
+++ kpn_m3/pinger.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+wait_time=5
+i=1
+repeat=3
+reached=false
+while [ $i -le $repeat ] && [ $reached == false ]; do 
+	echo "Ping attempt #${i}"  
+	if ! ping $1; then
+		reached=false
+		((i++))
+		echo "Ping failed!"
+		if [ $i -le $repeat ]; then
+			echo "Waiting $wait_time seconds before retry"
+			sleep ${wait_time}
+		else
+			echo "Ping failed after ${i} attempts. Stopping."
+		fi
+	else
+		reached=true
+		echo "Ping succeeded!"
+	fi
+done
