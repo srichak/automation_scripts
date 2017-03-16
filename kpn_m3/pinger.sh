9cebee923961925bf8d61bd1f61c28ca4674834a
k.bernatovics <k.bernatovics@accenture.com>
2017-03-16 09:38:43 +0000

    Update pinger.sh

--- kpn_m3/pinger.sh
+++ kpn_m3/pinger.sh
@@ -3,17 +3,17 @@ wait_time=10
 i=1
 repeat=3
 reached=false
-while [ $i -le $repeat ] && [ $reached=false ]; do 
+while [ $i -le $repeat ] && [ $reached = false ]; do 
 	echo "Ping attempt #${i}"  
-	if ! ping $1; then
+	if ! ping $1 -c 3; then
 		reached=false
-		((i++))
+		i=$((i+1))
 		echo "Ping failed!"
 		if [ $i -le $repeat ]; then
 			echo "Waiting $wait_time seconds before retry"
 			sleep ${wait_time}
 		else
-			echo "Ping failed after ${i} attempts. Stopping."
+			echo "Ping failed after $((i-1)) attempts. Stopping."
 		fi
 	else
 		reached=true
