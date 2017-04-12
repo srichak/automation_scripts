44a15ce7af9e1ed175cc13c34a5d376cc02d2d04
Kumar Biswas <biplab.kumar.biswas@accenture.com>
2017-04-12 10:02:40 +0530

    just a commit

--- AWS_CLI/Ambari_Scripts/ambari_wrapper.sh
+++ AWS_CLI/Ambari_Scripts/ambari_wrapper.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
 
+Ambari_user=${Ambari_user}
+Ambari_pass=${Ambari_pass}
+Ambari_Gateway_IP=${Ambari_Gateway_IP}
+
+
+
 echo "Starting Ambari EC2 instances..."
 ./startAmbariInstances.sh
 sleep 300
 echo "Starting Ambari services..."
-curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop HDFS via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://34.248.157.73:8080/api/v1/clusters/ava-batch/services
+#curl -u $Ambari_user:$Ambari_pass -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop HDFS via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://34.248.157.73:8080/api/v1/clusters/ava-batch/services
+curl -u $Ambari_user:$Ambari_pass -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop HDFS via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$Ambari_Gateway_IP:8080/api/v1/clusters/ava-batch/services
